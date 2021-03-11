c452c6439e858d3776540618c8b3c7637a60095b
Flaviu Nemes <nemes.flaviu@gmail.com>
2021-03-11 23:27:24 +0200

    Started the authentication section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import Authentication from './pages/authentication/authentication.component';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
@@ -12,6 +13,7 @@ function App() {
 			<Switch>
 				<Route exact path='/' component={HomePage} />
 				<Route exact path='/shop' component={ShopPage} />
+				<Route exact path='/signin' component={Authentication} />
 			</Switch>
 		</div>
 	);
