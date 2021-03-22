39645c46f7c1c9ad97118d250ceadcf228866706
Flaviu Nemes <nemes.flaviu@gmail.com>
2021-03-22 17:18:12 +0200

    Added Some Optimisations

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,19 @@
-import React, { useEffect } from 'react';
-import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component';
-import CheckoutPage from './pages/checkout/checkout.component';
+import React, { useEffect, lazy, Suspense } from 'react';
 import Header from './components/header/header.component';
-import Authentication from './pages/authentication/authentication.component';
 import { connect } from 'react-redux';
 import { setCurrentUser } from './redux/user/user.actions';
-
 import { Switch, Route, Redirect } from 'react-router-dom';
-import './App.css';
+import { GlobalStyle } from './global.styles';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import Spinner from './components/spinner/spinner.component';
+import ErrorBoundary from './components/error-boundary/error-boundary.component';
+
+const HomePage = lazy(() => import('./pages/homepage/homepage.component'));
+const ShopPage = lazy(() => import('./pages/shop/shop.component'));
+const CheckoutPage = lazy(() => import('./pages/checkout/checkout.component'));
+const Authentication = lazy(() =>
+	import('./pages/authentication/authentication.component')
+);
 
 const App = ({ setCurrentUser, currentUser }) => {
 	// const unsubscribeFromAuth = null;
@@ -34,18 +38,23 @@ const App = ({ setCurrentUser, currentUser }) => {
 
 	return (
 		<div>
+			<GlobalStyle />
 			<Header />
 			<Switch>
-				<Route exact path='/' component={HomePage} />
-				<Route path='/shop' component={ShopPage} />
-				<Route exact path='/checkout' component={CheckoutPage} />
-				<Route
-					exact
-					path='/signin'
-					render={() =>
-						currentUser ? <Redirect to='/' /> : <Authentication />
-					}
-				/>
+				<ErrorBoundary>
+					<Suspense fallback={<div>...Loading</div>}>
+						<Route exact path='/' component={HomePage} />
+						<Route path='/shop' component={ShopPage} />
+						<Route exact path='/checkout' component={CheckoutPage} />
+						<Route
+							exact
+							path='/signin'
+							render={() =>
+								currentUser ? <Redirect to='/' /> : <Authentication />
+							}
+						/>
+					</Suspense>
+				</ErrorBoundary>
 			</Switch>
 		</div>
 	);
